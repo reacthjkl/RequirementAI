35d4269d14a31a52aeb9ec8081a11bfec17aeaa1
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2026-04-18 09:32:30 +0200

    update user table

---
+++ API/RequirementAI.Persistence/Migrations/20260418073210_UpdateUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RequirementAI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Provider",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Provider",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
