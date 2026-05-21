50cbf2b3fa26df2d9bd2b48650398e55bc425ee0
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2026-05-21 14:14:32 +0200

    fix small issues

--- Frontend/src/app/shared/models/project-for-update.model.ts
+++ Frontend/src/app/shared/models/project-for-update.model.ts
@@ -1,4 +1,5 @@
 export interface ProjectForUpdate {
+  id: string;
   name: string;
   description: string;
 }
--- Frontend/src/app/shared/services/organization.ts
+++ Frontend/src/app/shared/services/organization.ts
@@ -3,11 +3,12 @@ import { Api } from '../../core/services/api';
 import { ApiController } from '../enums/api-controller.enum';
 import { ApiResponse } from '../models/api-response.model';
 import { OrganizationForUpdate } from '../models/organization-for-update.model';
+import { Organization } from './../models/organization.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Organization {
+export class OrganizationService {
   constructor(private api: Api) {}
 
   public async get(): Promise<Organization | null> {
