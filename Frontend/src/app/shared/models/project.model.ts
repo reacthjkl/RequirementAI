8d7ad2525a425ada1e4e4beb262889a4b900ec58
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2026-05-21 16:47:23 +0200

    calling finish project endpoint when user clicks finish

--- Frontend/src/app/shared/models/project.model.ts
+++ Frontend/src/app/shared/models/project.model.ts
@@ -1,6 +1,9 @@
+import { ProjectStatus } from '../enums/project-status.enum';
+
 export interface Project {
   id: string;
   name: string;
   description: string;
   createdAt: string;
+  status: ProjectStatus;
 }
