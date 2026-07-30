a07438c90b433d6b2270e58ec1dbe1ecf3709079
Illia Soloviov <74905269+reacthjkl@users.noreply.github.com>
2026-07-30 13:26:21 +0200

    remove unused temperature field of llm reuqest dto

--- API/RequirementAI.Contract/Dto/LLMDtos/LLMRequestDto.cs
+++ API/RequirementAI.Contract/Dto/LLMDtos/LLMRequestDto.cs
@@ -4,10 +4,8 @@ namespace RequirementAI.Contract.Dto.LLMDtos;
 
 public class LLMRequestDto(
     string prompt,
-    LLMRequestPurpose purpose,
-    float temperature = 2f)
+    LLMRequestPurpose purpose)
 {
     public string Prompt { get; set; } = prompt;
     public LLMRequestPurpose Purpose { get; set; } = purpose;
-    public float Temperature { get; set; } = temperature;
 }
